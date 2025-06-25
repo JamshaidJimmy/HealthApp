d57bfb2feeb883f9a0219595672a63d235f47ded
Jamshaid Jimmy <jamshaid.aslam903@gmail.com>
2025-06-26 00:53:22 +0530

    Setup Expo web export and webpack config fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,28 @@
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+const webpack = require('webpack');
 
 module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
-  // Customize the config before returning it.
+ config.resolve.fallback = {
+    ...config.resolve.fallback,
+    crypto: require.resolve('crypto-browserify'),
+    stream: require.resolve('stream-browserify'),
+    buffer: require.resolve('buffer/'),
+    assert: require.resolve('assert/'),
+    util: require.resolve('util/'),
+    process: require.resolve('process/browser'),
+    vm: false,
+  };
+
+  // Add necessary plugins (if not already)
+  const webpack = require('webpack');
+  config.plugins.push(
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+      process: 'process/browser',
+    })
+  );
+
+
   return config;
 };
